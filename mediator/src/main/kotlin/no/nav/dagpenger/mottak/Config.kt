b3d1c07fc2192713e9b0cc37b34de8723bb70988
Giao The Cung <giaothe@gmail.com>
2021-05-05 09:17:19 +0200

    Forbredelse til å gå mot pdl direkte
    Co-authored-by: rannveigskjerve <rannveig.alette.skjerve@nav.no>
    Co-authored-by: Geir Andre Lund <geir.andre.lund@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/mottak/Config.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/mottak/Config.kt
@@ -9,7 +9,6 @@ import com.natpryce.konfig.intType
 import com.natpryce.konfig.overriding
 import com.natpryce.konfig.stringType
 import com.zaxxer.hikari.HikariDataSource
-import io.netty.util.NetUtil.getHostname
 import no.finn.unleash.DefaultUnleash
 import no.finn.unleash.util.UnleashConfig
 import no.nav.dagpenger.aad.api.ClientCredentialsClient
@@ -20,11 +19,11 @@ internal object Config {
 
     private val defaultProperties = ConfigurationMap(
         mapOf(
-            "DB_USERNAME" to "username",
-            "DB_PASSWORD" to "password",
             "DB_DATABASE" to "dp-mottak",
             "DB_HOST" to "localhost",
+            "DB_PASSWORD" to "password",
             "DB_PORT" to "5432",
+            "DB_USERNAME" to "username",
             "DP_PROXY_SCOPE" to "api://dev-fss.teamdagpenger.dp-proxy/.default",
             "DP_PROXY_URL" to "https://dp-proxy.dev-fss-pub.nais.io",
             "HTTP_PORT" to "8080",
@@ -32,6 +31,8 @@ internal object Config {
             "KAFKA_EXTRA_TOPIC" to "teamdagpenger.mottak.v1,teamdagpenger.regel.v1",
             "KAFKA_RAPID_TOPIC" to "teamdagpenger.rapid.v1",
             "KAFKA_RESET_POLICY" to "latest",
+            "PDL_API_SCOPE" to "api://dev-fss.pdl.pdl-api/.default",
+            "PDL_API_URL" to "https://pdl-api.dev-fss-pub.nais.io",
             "UNLEASH_URL" to "https://unleash.nais.io/api/"
 
         )
@@ -39,7 +40,9 @@ internal object Config {
     private val prodProperties = ConfigurationMap(
         mapOf(
             "DP_PROXY_SCOPE" to "api://prod-fss.teamdagpenger.dp-proxy/.default",
-            "DP_PROXY_URL" to "https://dp-proxy.prod-fss-pub.nais.io"
+            "DP_PROXY_URL" to "https://dp-proxy.prod-fss-pub.nais.io",
+            "PDL_API_SCOPE" to "api://prod-fss.pdl.pdl-api/.default",
+            "PDL_API_URL" to "https://pdl-api.prod-fss-pub.nais.io",
         )
     )
 
@@ -55,6 +58,7 @@ internal object Config {
         ClientCredentialsClient(properties) {
             scope {
                 add(properties.dpProxyScope())
+                add(properties.pdlApiScope())
             }
         }
     }
@@ -77,6 +81,7 @@ internal object Config {
 
     fun Configuration.dpProxyUrl() = this[Key("DP_PROXY_URL", stringType)]
     fun Configuration.dpProxyScope() = this[Key("DP_PROXY_SCOPE", stringType)]
+    fun Configuration.pdlApiScope() = this[Key("PDL_API_SCOPE", stringType)]
     fun unleash() = DefaultUnleash(unleashConfig(), ByClusterStrategy(ByClusterStrategy.Cluster.current))
     fun unleashConfig(): UnleashConfig =
         UnleashConfig.builder()
@@ -84,6 +89,7 @@ internal object Config {
             .instanceId(getHostname())
             .unleashAPI(properties[Key("UNLEASH_URL", stringType)])
             .build()
+
     fun asMap(): Map<String, String> = properties.list().reversed().fold(emptyMap()) { map, pair ->
         map + pair.second
     }
