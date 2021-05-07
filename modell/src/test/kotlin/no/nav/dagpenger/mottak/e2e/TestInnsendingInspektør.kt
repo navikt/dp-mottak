2ef4616a55290465e81eb847267eae41193fc5b1
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2021-05-07 09:12:00 +0200

    dokumentasjon: skriv plantuml til fil

--- modell/src/test/kotlin/no/nav/dagpenger/mottak/e2e/TestInnsendingInspektør.kt
+++ modell/src/test/kotlin/no/nav/dagpenger/mottak/e2e/TestInnsendingInspektør.kt
@@ -5,6 +5,8 @@ import no.nav.dagpenger.mottak.Innsending
 import no.nav.dagpenger.mottak.InnsendingObserver
 import no.nav.dagpenger.mottak.InnsendingTilstandType
 import no.nav.dagpenger.mottak.InnsendingVisitor
+import java.io.File
+import java.nio.file.Paths
 
 class TestInnsendingInspektør(innsending: Innsending) : InnsendingVisitor {
 
@@ -24,11 +26,15 @@ class TestInnsendingInspektør(innsending: Innsending) : InnsendingVisitor {
     }
 }
 
-class PlantUmlObservatør():InnsendingObserver{
+class PlantUmlObservatør() : InnsendingObserver {
     internal val tilstander = mutableListOf<String>()
 
-    override fun tilstandEndret(event: InnsendingObserver.InnsendingEndretTilstandEvent){
+    override fun tilstandEndret(event: InnsendingObserver.InnsendingEndretTilstandEvent) {
         tilstander.add("${event.forrigeTilstand} --> ${event.gjeldendeTilstand.name}")
+    }
+
+    fun reset() {
+        tilstander.clear()
     }
 
     fun toPlantUml(brevkode: String): String =
@@ -42,8 +48,16 @@ class PlantUmlObservatør():InnsendingObserver{
           |${tilstander.sisteTilstand()}--> [*]
           |@enduml
       """.trimMargin()
+
+    fun writePlantUml(brevkode: String, subPath: String? = null) {
+        println(Paths.get("").toAbsolutePath().toString())
+        var path = "${Paths.get("").toAbsolutePath().toString().substringBeforeLast("/")}/docs/arkitektur/tilstander"
+        if (subPath != null)
+            path += "/$subPath"
+
+        File("$path/$brevkode.puml").writeText(toPlantUml(brevkode))
+    }
 }
 
 private fun MutableList<String>.førsteTilstand(): String = this.first().substringBefore("--> ")
 private fun MutableList<String>.sisteTilstand(): String = this.last().substringAfter("--> ")
-
