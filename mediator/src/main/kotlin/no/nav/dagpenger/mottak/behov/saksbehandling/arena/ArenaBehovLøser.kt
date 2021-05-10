28cdcc5b94af3a81162c4d505c09c36dbd2ab00c
Giao The Cung <giaothe@gmail.com>
2021-05-10 15:12:03 +0200

    Skip another jp

--- mediator/src/main/kotlin/no/nav/dagpenger/mottak/behov/saksbehandling/arena/ArenaBehovLøser.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/mottak/behov/saksbehandling/arena/ArenaBehovLøser.kt
@@ -88,7 +88,7 @@ internal class ArenaBehovLøser(arenaOppslag: ArenaOppslag, rapidsConnection: Ra
             try {
                 runBlocking {
                     val behovNavn = packet["@behov"].first().asText()
-                    if (journalpostId == "506723123") {
+                    if (journalpostId == "506726563") {
                         packet["@feil"] = behovNavn
                         logger.error { "Kunne ikke opprette arena sak med journalpostId $journalpostId" }
                     } else {
