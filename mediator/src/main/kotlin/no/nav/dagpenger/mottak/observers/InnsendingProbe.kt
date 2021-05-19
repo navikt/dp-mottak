f6e7139a217327db288ecf275df0d4826f83ce07
Geir André Lund <geir.andre.lund@nav.no>
2021-05-19 12:23:56 +0200

    Mer logging av hvilken kategorisering innsendingen hadde

--- mediator/src/main/kotlin/no/nav/dagpenger/mottak/observers/InnsendingProbe.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/mottak/observers/InnsendingProbe.kt
@@ -15,7 +15,7 @@ internal object InnsendingProbe : InnsendingObserver {
 
     override fun innsendingFerdigstilt(event: InnsendingObserver.InnsendingEvent) {
         log.info {
-            "Innsending ${event.journalpostId} ferdigstilt. Ble sendt til behandlende enhet ${event.behandlendeEnhet}"
+            "Innsending ${event.journalpostId} ferdigstilt. Kategorisert som ${event.type}, ble sendt til behandlende enhet ${event.behandlendeEnhet}"
         }
     }
 }
