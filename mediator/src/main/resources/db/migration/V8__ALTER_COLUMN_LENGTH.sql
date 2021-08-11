a0daa01c46f2b325f0c9d309f74bd00a11bff60f
Giao The Cung <giaothe@gmail.com>
2021-08-11 12:59:19 +0200

    Endre lengde på kolonne
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
    Co-authored-by: Geir Andre Lund <geir.andre.lund@nav.no>

---
+++ mediator/src/main/resources/db/migration/V8__ALTER_COLUMN_LENGTH.sql
@@ -0,0 +1,2 @@
+ALTER TABLE IF EXISTS journalpost_dokumenter_v1
+    ALTER COLUMN tittel TYPE VARCHAR(500);
--- mediator/src/test/kotlin/no/nav/dagpenger/mottak/db/SkjemaTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/mottak/db/SkjemaTest.kt
@@ -10,7 +10,7 @@ internal class SkjemaTest {
     @Test
     fun `riktig anntall migreringer`() {
         withCleanDb {
-            assertEquals(7, runMigration(dataSource))
+            assertEquals(8, runMigration(dataSource))
         }
     }
 }
