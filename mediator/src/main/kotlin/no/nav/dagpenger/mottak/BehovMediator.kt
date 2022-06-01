a336d6a1c13b26220e9a81098f21a486bb5ed392
Geir André Lund <geir.andre.lund@nav.no>
2022-06-01 15:58:59 +0200

    Fjernet ubrukt id.

--- mediator/src/main/kotlin/no/nav/dagpenger/mottak/BehovMediator.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/mottak/BehovMediator.kt
@@ -4,7 +4,6 @@ import mu.KLogger
 import mu.KotlinLogging
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.RapidsConnection
-import java.util.UUID
 
 private val logg = KotlinLogging.logger {}
 
@@ -23,23 +22,22 @@ class BehovMediator(
     ) {
         behov.groupBy { it.kontekst() }.forEach { (kontekst, behov) ->
             val behovsliste = mutableListOf<String>()
-            val id = UUID.randomUUID()
 
             mutableMapOf<String, Any>().apply {
-                    putAll(kontekst)
-                    behov.forEach { behov ->
-                        require(behov.type.name !in behovsliste) { "Kan ikke produsere samme behov ${behov.type.name} på samme kontekst" }
-                        require(
-                            behov.detaljer().filterKeys { this.containsKey(it) && this[it] != behov.detaljer()[it] }
-                                .isEmpty()
-                        ) { "Kan ikke produsere behov med duplikate detaljer" }
-                        behovsliste.add(behov.type.name)
-                        putAll(behov.detaljer())
-                    }
+                putAll(kontekst)
+                behov.forEach { behov ->
+                    require(behov.type.name !in behovsliste) { "Kan ikke produsere samme behov ${behov.type.name} på samme kontekst" }
+                    require(
+                        behov.detaljer().filterKeys { this.containsKey(it) && this[it] != behov.detaljer()[it] }
+                            .isEmpty()
+                    ) { "Kan ikke produsere behov med duplikate detaljer" }
+                    behovsliste.add(behov.type.name)
+                    putAll(behov.detaljer())
                 }
+            }
                 .let { JsonMessage.newNeed(behovsliste, it) }
                 .also { message ->
-                    sikkerLogg.info { "Sender $id som ${message.toJson()}" }
+                    sikkerLogg.info { "Sender behov for ${hendelse.journalpostId()} ${message.toJson()}" }
                     rapidsConnection.publish(hendelse.journalpostId(), message.toJson())
                     logg.info { "Sender behov ${behovsliste.joinToString { it }}" }
                 }
