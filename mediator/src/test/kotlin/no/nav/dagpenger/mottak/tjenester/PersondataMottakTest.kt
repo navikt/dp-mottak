f085e82af080b0d80c7b646047364b6cc63b2d2f
Giao The Cung <giaothe@gmail.com>
2023-02-03 12:41:23 +0100

    Håndter pakker der persondata er null

--- mediator/src/test/kotlin/no/nav/dagpenger/mottak/tjenester/PersondataMottakTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/mottak/tjenester/PersondataMottakTest.kt
@@ -1,29 +1,39 @@
 package no.nav.dagpenger.mottak.tjenester
 
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
 import no.nav.dagpenger.mottak.InnsendingMediator
-import no.nav.dagpenger.mottak.db.InMemoryInnsendingRepository
-import no.nav.dagpenger.mottak.e2e.TestObservatør
+import no.nav.dagpenger.mottak.meldinger.PersonInformasjonIkkeFunnet
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import org.junit.Before
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
 import java.util.UUID
 
 class PersondataMottakTest {
-    val testRapid = TestRapid()
-    private val innsendingRepository = InMemoryInnsendingRepository()
-    private val observatør = TestObservatør()
-    private val innsendingMediator = InnsendingMediator(
-        innsendingRepository = innsendingRepository,
-        rapidsConnection = testRapid,
-        observatører = listOf(observatør)
-    )
+    private val testRapid = TestRapid()
+    private val innsendingMediator = mockk<InnsendingMediator>().also {
+        every { it.håndter(any<PersonInformasjonIkkeFunnet>()) } just Runs
+    }
+
     init {
         PersondataMottak(innsendingMediator, testRapid)
+    }
+
+    @Before
+    fun setup() {
+        testRapid.reset()
     }
 
     @Test
     fun `håndterer svar med null`() {
         testRapid.sendTestMessage(persondataMottattHendelse())
+        verify(exactly = 1) {
+            innsendingMediator.håndter(any<PersonInformasjonIkkeFunnet>())
+        }
     }
 
     //language=JSON
