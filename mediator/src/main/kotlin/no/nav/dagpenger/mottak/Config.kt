272f30ecb752aa1fc13a304a9339429a940d4097
Giao The Cung <giaothe@gmail.com>
2023-04-12 11:51:09 +0200

    Legge til outbound external host rule
    DAG-803

--- mediator/src/main/kotlin/no/nav/dagpenger/mottak/Config.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/mottak/Config.kt
@@ -28,14 +28,12 @@ internal object Config {
             "DB_PORT" to "5432",
             "DB_USERNAME" to "username",
             "DP_PROXY_SCOPE" to "api://dev-fss.teamdagpenger.dp-proxy/.default",
-            "DP_PROXY_URL" to "https://dp-proxy.dev-fss-pub.nais.io",
             "HTTP_PORT" to "8080",
             "KAFKA_CONSUMER_GROUP_ID" to "dp-mottak-v1",
             "KAFKA_EXTRA_TOPIC" to "teamdagpenger.mottak.v1,teamdagpenger.regel.v1",
             "KAFKA_RAPID_TOPIC" to "teamdagpenger.rapid.v1",
             "KAFKA_RESET_POLICY" to "latest",
             "PDL_API_SCOPE" to "api://dev-fss.pdl.pdl-api/.default",
-            "PDL_API_URL" to "https://pdl-api.dev-fss-pub.nais.io",
             "SKJERMING_API_SCOPE" to "api://dev-gcp.nom.skjermede-personer-pip/.default",
             "SKJERMING_API_URL" to "http://skjermede-personer-pip.nom/skjermet",
             "UNLEASH_URL" to "https://unleash.nais.io/api/",
@@ -48,9 +46,7 @@ internal object Config {
     private val prodProperties = ConfigurationMap(
         mapOf(
             "DP_PROXY_SCOPE" to "api://prod-fss.teamdagpenger.dp-proxy/.default",
-            "DP_PROXY_URL" to "https://dp-proxy.prod-fss-pub.nais.io",
             "PDL_API_SCOPE" to "api://prod-fss.pdl.pdl-api/.default",
-            "PDL_API_URL" to "https://pdl-api.prod-fss-pub.nais.io",
             "SKJERMING_API_SCOPE" to "api://prod-gcp.nom.skjermede-personer-pip/.default",
         )
     )
@@ -70,6 +66,8 @@ internal object Config {
             authType = azureAd.clientSecret(),
         )
     }
+
+    private fun String.addHttprotocoll(): String = "https://$this"
 
     val Configuration.dpProxyTokenProvider: () -> String by lazy {
         { cachedTokenProvider.clientCredentials(properties[Key("DP_PROXY_SCOPE", stringType)]).accessToken }
@@ -104,8 +102,8 @@ internal object Config {
     }
 
     fun Configuration.skjermingApiUrl() = this[Key("SKJERMING_API_URL", stringType)]
-    fun Configuration.dpProxyUrl() = this[Key("DP_PROXY_URL", stringType)]
-    fun Configuration.pdlApiUrl() = this[Key("PDL_API_URL", stringType)]
+    fun Configuration.dpProxyUrl() = this[Key("DP_PROXY_HOST", stringType)].addHttprotocoll()
+    fun Configuration.pdlApiUrl() = this[Key("PDL_API_HOST", stringType)].addHttprotocoll()
     fun unleash() = DefaultUnleash(unleashConfig(), ByClusterStrategy(ByClusterStrategy.Cluster.current))
     private fun unleashConfig(): UnleashConfig =
         UnleashConfig.builder()
