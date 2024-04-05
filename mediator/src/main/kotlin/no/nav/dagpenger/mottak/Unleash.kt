b2261e6b0a4b315ac18cc228db32bbdcf9d60a43
André Roaldseth <andre.roaldseth@nav.no>
2024-04-05 12:47:53 +0200

    Hent ut om det er quiz eller behandling som skal automatisere

---
+++ mediator/src/main/kotlin/no/nav/dagpenger/mottak/Unleash.kt
@@ -0,0 +1,25 @@
+package no.nav.dagpenger.mottak
+
+import com.natpryce.konfig.Key
+import com.natpryce.konfig.stringType
+import io.getunleash.DefaultUnleash
+import io.getunleash.Unleash
+import io.getunleash.util.UnleashConfig
+import no.nav.dagpenger.mottak.Config.properties
+import java.net.InetAddress
+
+val unleash: Unleash by lazy {
+    DefaultUnleash(
+        UnleashConfig.builder()
+            .appName(properties[Key("NAIS_APP_NAME", stringType)])
+            .instanceId(runCatching { InetAddress.getLocalHost().hostName }.getOrElse { "ukjent" })
+            .unleashAPI(properties[Key("UNLEASH_SERVER_API_URL", stringType)] + "/api/")
+            .apiKey(properties[Key("UNLEASH_SERVER_API_TOKEN", stringType)])
+            .environment(
+                when (System.getenv("NAIS_CLUSTER_NAME").orEmpty()) {
+                    "prod-gcp" -> "production"
+                    else -> "development"
+                },
+            ).build(),
+    )
+}
