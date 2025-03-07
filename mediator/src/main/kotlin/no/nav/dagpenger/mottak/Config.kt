84de0b25cb7d2197f59f566a98044c2be5a23521
Geir André Lund <geir.andre.lund@nav.no>
2025-03-07 14:57:49 +0100

    riktig PDL q1 scope.

--- mediator/src/main/kotlin/no/nav/dagpenger/mottak/Config.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/mottak/Config.kt
@@ -32,7 +32,7 @@ internal object Config {
                 "KAFKA_EXTRA_TOPIC" to "teamdagpenger.mottak.v1,teamdagpenger.regel.v1",
                 "KAFKA_RAPID_TOPIC" to "teamdagpenger.rapid.v1",
                 "KAFKA_RESET_POLICY" to "LATEST",
-                "PDL_API_SCOPE" to "api://dev-fss.pdl.pdl-api/.default",
+                "PDL_API_SCOPE" to "api://dev-fss.pdl.pdl-api-q1/.default",
                 "SKJERMING_API_SCOPE" to "api://dev-gcp.nom.skjermede-personer-pip/.default",
                 "SKJERMING_API_URL" to "http://skjermede-personer-pip.nom/skjermet",
                 "AZURE_OPENID_CONFIG_ISSUER" to "azureAd",
