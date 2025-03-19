bf813a20af907ac85f8849f243d2be120d9be3e2
Giao The Cung <giaothe@gmail.com>
2025-03-19 21:07:20 +0100

    Fix openapi yaml

--- mediator/src/main/kotlin/no/nav/dagpenger/mottak/api/JournalpostApi.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/mottak/api/JournalpostApi.kt
@@ -7,12 +7,8 @@ import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import no.nav.dagpenger.mottak.Config
-import no.nav.dagpenger.mottak.InnsendingObserver
-import no.nav.dagpenger.mottak.db.InnsendingRepository
 
-internal fun Application.journalpostApi(
-
-) {
+internal fun Application.journalpostApi() {
     routing {
         authenticate(Config.AzureAd.NAME) {
             get("v1/journalpost/{soknadId}") {
@@ -21,4 +17,5 @@ internal fun Application.journalpostApi(
                 }
             }
         }
-    }}
+    }
+}
