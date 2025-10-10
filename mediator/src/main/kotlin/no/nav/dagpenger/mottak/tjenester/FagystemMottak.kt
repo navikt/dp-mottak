72700efa256075f8b77f49eb44daebf770f72cba
Giao The Cung <giaothe@gmail.com>
2025-10-10 10:14:50 +0200

    Reverte språkrådet
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

---
+++ mediator/src/main/kotlin/no/nav/dagpenger/mottak/tjenester/FagystemMottak.kt
@@ -0,0 +1,75 @@
+package no.nav.dagpenger.mottak.tjenester
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
+import com.github.navikt.tbd_libs.rapids_and_rivers.River
+import com.github.navikt.tbd_libs.rapids_and_rivers.asLocalDateTime
+import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageContext
+import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageMetadata
+import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
+import io.github.oshai.kotlinlogging.KotlinLogging
+import io.micrometer.core.instrument.MeterRegistry
+import no.nav.dagpenger.mottak.Aktivitetslogg
+import no.nav.dagpenger.mottak.Aktivitetslogg.Aktivitet.Behov.Behovtype
+import no.nav.dagpenger.mottak.Fagsystem
+import no.nav.dagpenger.mottak.InnsendingMediator
+import no.nav.dagpenger.mottak.meldinger.FagsystemBesluttet
+import no.nav.dagpenger.mottak.serder.asUUID
+
+private val logg = KotlinLogging.logger {}
+
+internal class FagystemMottak(
+    private val innsendingMediator: InnsendingMediator,
+    rapidsConnection: RapidsConnection,
+) : River.PacketListener {
+    init {
+        River(rapidsConnection)
+            .apply {
+                precondition { it.requireValue("@event_name", "behov") }
+                precondition { it.requireValue("@final", true) }
+                precondition {
+                    it.requireAll("@behov", listOf(Behovtype.BestemFagsystem.name))
+                }
+                validate { it.require("@opprettet", JsonNode::asLocalDateTime) }
+                validate { it.requireKey("@løsning") }
+                validate { it.requireKey("journalpostId") }
+                // TODO: Skal denne være required? Eller må vi håndtere at behovet er løst med @feil?
+                validate { it.requireKey("fagsystem") }
+                validate { it.interestedIn("fagsakId") }
+            }.register(this)
+    }
+
+    private fun JsonMessage.fagsystem(): Fagsystem {
+        val fagsystemType =
+            try {
+                Fagsystem.FagsystemType.valueOf(this["fagsystem"].asText())
+            } catch (e: IllegalArgumentException) {
+                throw IllegalArgumentException("Ukjent fagsystem: ${this["fagsystem"].asText()}")
+            }
+        return when (fagsystemType) {
+            Fagsystem.FagsystemType.DAGPENGER -> Fagsystem.Dagpenger(this["fagsakId"].asUUID())
+            Fagsystem.FagsystemType.ARENA -> Fagsystem.Arena
+        }
+    }
+
+    override fun onPacket(
+        packet: JsonMessage,
+        context: MessageContext,
+        metadata: MessageMetadata,
+        meterRegistry: MeterRegistry,
+    ) {
+        val løsning = packet["@løsning"].first()
+        val journalpostId = packet["journalpostId"].asText()
+        val fagsystem = packet["fagsystem"].asText()
+        logg.info { "Fått løsning for ${packet["@behov"].map { it.asText() }}, journalpostId: $journalpostId. Fagsystem er $fagsystem" }
+
+        val fagsystemBesluttet =
+            FagsystemBesluttet(
+                aktivitetslogg = Aktivitetslogg(),
+                journalpostId = journalpostId,
+                fagsystem = packet.fagsystem(),
+            )
+
+        innsendingMediator.håndter(fagsystemBesluttet)
+    }
+}
