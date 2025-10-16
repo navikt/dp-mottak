7b7f34548a34dbc04b20c68d6d3b0f393865c7db
Giao The Cung <giaothe@gmail.com>
2025-10-16 11:06:41 +0200

    Mer logging
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/mottak/observers/FerdigstiltEttersendingObserver.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/mottak/observers/FerdigstiltEttersendingObserver.kt
@@ -1,6 +1,7 @@
 package no.nav.dagpenger.mottak.observers
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import io.github.oshai.kotlinlogging.withLoggingContext
 import kotlinx.coroutines.runBlocking
 import no.nav.dagpenger.mottak.InnsendingObserver
 import no.nav.dagpenger.mottak.behov.saksbehandling.SaksbehandlingKlient
@@ -15,6 +16,7 @@ import no.nav.dagpenger.mottak.meldinger.søknadsdata.QuizSøknadFormat
 import java.time.LocalDate
 
 private val sikkerlogg = KotlinLogging.logger("tjenestekall.FerdigstiltEttersendingObserver")
+private val logger = KotlinLogging.logger { }
 
 class FerdigstiltEttersendingObserver internal constructor(
     private val saksbehandlingKlient: SaksbehandlingKlient,
@@ -42,28 +44,34 @@ class FerdigstiltEttersendingObserver internal constructor(
         val ident: String = requireNotNull(event.fødselsnummer)
         val aktørId: String = requireNotNull(event.aktørId)
         runBlocking {
-            when (saksbehandlingKlient.skalVarsleOmEttersending(søknadId = søknadId, ident = ident)) {
-                true ->
-                    runBlocking {
-                        val oppgave =
-                            GosysOppgaveRequest(
-                                journalpostId = event.journalpostId,
-                                aktoerId = aktørId,
-                                tildeltEnhetsnr = event.behandlendeEnhet,
-                                aktivDato = LocalDate.now(),
-                                beskrivelse = "Ettersendelse til dagpengesøknad i ny løsning",
-                                oppgavetype = "ETTERSEND_MOTT",
-                            )
-                        sikkerlogg.info { "Oppretter gosys oppgave: $oppgave for søknaId: $søknadId og ident: $ident" }
-                        gosysClient.opprettOppgave(
-                            oppgave =
-                            oppgave,
-                        ).also {
-                            sikkerlogg.info { "Opprettet gosys oppgave for søknaId: $søknadId og ident: $ident og gosys id: $it " }
+            withLoggingContext("journalpostId" to event.journalpostId, "fagsakId" to "${event.fagsakId}") {
+                when (saksbehandlingKlient.skalVarsleOmEttersending(søknadId = søknadId, ident = ident)) {
+                    true ->
+                        runBlocking {
+                            val oppgave =
+                                GosysOppgaveRequest(
+                                    journalpostId = event.journalpostId,
+                                    aktoerId = aktørId,
+                                    tildeltEnhetsnr = event.behandlendeEnhet,
+                                    aktivDato = LocalDate.now(),
+                                    beskrivelse = "Ettersendelse til dagpengesøknad i ny løsning",
+                                    oppgavetype = "ETTERSEND_MOTT",
+                                )
+                            logger.info { "Lager gosys varsel oppgave ettersending" }
+                            sikkerlogg.info { "Oppretter gosys oppgave: $oppgave for søknaId: $søknadId og ident: $ident" }
+                            gosysClient.opprettOppgave(
+                                oppgave =
+                                    oppgave,
+                            ).also {
+                                sikkerlogg.info { "Opprettet gosys oppgave for søknaId: $søknadId og ident: $ident og gosys id: $it " }
+                            }
                         }
-                    }
 
-                else -> {}
+                    else -> {
+                        logger.info { "Lager IKKE gosys varsel oppgave ettersending" }
+                    }
+                }
+
             }
         }
     }
