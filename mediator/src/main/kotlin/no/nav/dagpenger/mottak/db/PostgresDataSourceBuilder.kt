280cb04e464336d349d586decd364417b1592652
Geir André Lund <geir.andre.lund@nav.no>
2026-06-24 12:45:33 +0200

    Bytt til JDBC-url for HikariCP

--- mediator/src/main/kotlin/no/nav/dagpenger/mottak/db/PostgresDataSourceBuilder.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/mottak/db/PostgresDataSourceBuilder.kt
@@ -2,44 +2,68 @@ package no.nav.dagpenger.mottak.db
 
 import ch.qos.logback.core.util.OptionHelper
 import com.zaxxer.hikari.HikariDataSource
+import io.micrometer.core.instrument.Clock
+import io.micrometer.prometheusmetrics.PrometheusConfig
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
+import io.prometheus.metrics.model.registry.PrometheusRegistry
 import org.flywaydb.core.Flyway
 import org.flywaydb.core.internal.configuration.ConfigUtils
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 
 internal object PostgresDataSourceBuilder {
     const val DB_USERNAME_KEY = "DB_USERNAME"
     const val DB_PASSWORD_KEY = "DB_PASSWORD"
-    const val DB_DATABASE_KEY = "DB_DATABASE"
-    const val DB_HOST_KEY = "DB_HOST"
-    const val DB_PORT_KEY = "DB_PORT"
+    const val DB_URL_KEY = "DB_URL"
 
     private fun getOrThrow(key: String): String = OptionHelper.getEnv(key) ?: OptionHelper.getSystemProperty(key) ?: throw IllegalArgumentException("Missing required environment variable $key")
 
-    val dataSource by lazy {
+    val dataSource =
         HikariDataSource().apply {
-            dataSourceClassName = "org.postgresql.ds.PGSimpleDataSource"
-            addDataSourceProperty("serverName", getOrThrow(DB_HOST_KEY))
-            addDataSourceProperty("portNumber", getOrThrow(DB_PORT_KEY))
-            addDataSourceProperty("databaseName", getOrThrow(DB_DATABASE_KEY))
-            addDataSourceProperty("user", getOrThrow(DB_USERNAME_KEY))
-            addDataSourceProperty("password", getOrThrow(DB_PASSWORD_KEY))
-            maximumPoolSize = 10
-            minimumIdle = 1
-            idleTimeout = 10001
-            connectionTimeout = 1000
-            maxLifetime = 30001
+            jdbcUrl = getOrThrow(DB_URL_KEY).ensurePrefix("jdbc:postgresql://").stripCredentials()
+            username = getOrThrow(DB_USERNAME_KEY)
+            password = getOrThrow(DB_PASSWORD_KEY)
+
+            // Default 30 sekund
+            connectionTimeout = 10.seconds.inWholeMilliseconds
+            // Default 10 minutter
+            idleTimeout = 10.minutes.inWholeMilliseconds
+            // Default 2 minutter
+            keepaliveTime = 2.minutes.inWholeMilliseconds
+            // Default 30 minutter
+            maxLifetime = 30.minutes.inWholeMilliseconds
+            leakDetectionThreshold = 10.seconds.inWholeMilliseconds
+            metricRegistry =
+                PrometheusMeterRegistry(
+                    PrometheusConfig.DEFAULT,
+                    PrometheusRegistry.defaultRegistry,
+                    Clock.SYSTEM,
+                )
         }
-    }
 
     internal fun clean() =
-        Flyway.configure()
+        Flyway
+            .configure()
             .cleanDisabled(getOrThrow(ConfigUtils.CLEAN_DISABLED).toBooleanStrict())
-            .dataSource(dataSource).load().clean()
+            .dataSource(dataSource)
+            .load()
+            .clean()
 
     internal fun runMigration(initSql: String? = null): Int =
-        Flyway.configure()
+        Flyway
+            .configure()
             .dataSource(dataSource)
             .initSql(initSql)
             .load()
             .migrate()
             .migrations.size
 }
+
+private fun String.ensurePrefix(prefix: String) =
+    if (this.startsWith(prefix)) {
+        this
+    } else {
+        prefix + this.substringAfter("//")
+    }
+
+private fun String.stripCredentials() = this.replace(Regex("://.*:.*@"), "://")
